197ddf97870281bae607d304e7a13a2bb45ddfda
shubham-1903 <shubhamkr7677@gmail.com>
2021-07-13 14:47:01 +0530

    working correctly

--- Divide_and_conquer/Max_Min_Algorithm.cpp
+++ Divide_and_conquer/Max_Min_Algorithm.cpp
@@ -15,7 +15,7 @@ void naive_max_min(vector<int> v,int n)
   cout <<"Maximum number is: "<< maximum << "\n" <<"Minimum number is: "<< minimum << "\n"; 
 }
 //? Divide and Conquer Approach
-void max_min(vector<int> v,int i,int j,int maximum,int minimum)
+void max_min(vector<int> v,int i,int j,int &maximum,int &minimum)
 {
   if(i==j) maximum = minimum = v[i];
   else if(i==j-1)
@@ -32,7 +32,7 @@ void max_min(vector<int> v,int i,int j,int maximum,int minimum)
     int mid=(i+j)/2;
     max_min(v,i,mid,maximum,minimum);
     int maximum1,minimum1;
-    max_min(v,mid+1,j,maximum1,minimum1);
+    max_min(v,mid+1,j,maximum1 ,minimum1);
 
     if(maximum < maximum1) maximum=maximum1;
     if(minimum > minimum1) minimum=minimum1;
@@ -42,9 +42,9 @@ void max_min(vector<int> v,int i,int j,int maximum,int minimum)
 int main()
 {
   vector<int> v={5,4,1,2,6};
-  naive_max_min(v,v.size());
-  int maximum=v[0],minimum=v[0];
-  max_min(v,0,5,maximum,minimum);
+  naive_max_min(v,v.size()); //?
+  int maximum=INT_MIN,minimum=INT_MAX;
+  max_min(v,0,v.size()-1,maximum,minimum);
   cout<<"Maximum is: "<< maximum <<"\n" <<"Minimum is: "<< minimum<<endl;
 
   return 0;
